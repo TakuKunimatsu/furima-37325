4478000128f7a82ced4c804871e5f8b47a1c422c
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-17 19:37:38 +0900

    Rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' } 
+  validates :nickname, presence: true
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-           validates :last_name
-           validates :first_name
-         end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
 
-         with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを使用してください' } do
-           validates :last_name_kana
-           validates :first_name_kana
-         end
-         
-         validates :birth_date, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
 
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'には全角カナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birth_date, presence: true
 end
