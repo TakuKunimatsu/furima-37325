2083cee13c1cb162c59b089d17fa9da12b799254
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-19 13:46:40 +0900

    itemsテーブルの作成、画像保存機能の実装、itemsコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,22 @@
 class ItemsController < ApplicationController
+
+   def index
+      @items = Item.all
+   end
+
+   def new
+      @item = Item.new
+   end
+
+   def create
+      @item = Item.create(item_params)
+   end
+
+   private
+
+   def item_params
+      params.require(:item).permit(:name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :scheduled_id, :price, :image).merge(user_id: current_user.id)
+   end
+   
 end
+
