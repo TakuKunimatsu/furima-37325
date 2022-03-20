46075d631fbaab6478a5bd3fedd9ebf33fd05044
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-20 20:34:06 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,19 @@ class Item < ApplicationRecord
 
   validates :name,            presence: true
   validates :info,            presence: true
-  validates :category_id,     presence: true
-  validates :status_id,       presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id,   presence: true
-  validates :scheduled_id,    presence: true
-  validates :price,           presence: true
+  validates :category_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_id,    presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,           presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                                                              format: { with: /\A[0-9]+\z/ }
   validates :image,           presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :scheduled
 end
