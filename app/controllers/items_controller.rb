3c0c6006e0d78cda15469c3e2b131416e975cebe
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-20 23:05:22 +0900

    rubocopによる整形

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,25 @@
 class ItemsController < ApplicationController
-   
-   def index
-      @items = Item.all
-   end
+  def index
+    @items = Item.all
+  end
 
-   def new
-      @item = Item.new
-   end
+  def new
+    @item = Item.new
+  end
 
-   def create
-      @item = Item.create(item_params)
-      if @item.save
-         redirect_to root_path
-       else
-         render :new
-       end
-   end
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
-   private
+  private
 
-   def item_params
-      params.require(:item).permit(:name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :scheduled_id, :price, :image).merge(user_id: current_user.id)
-   end   
+  def item_params
+    params.require(:item).permit(:name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :scheduled_id,
+                                 :price, :image).merge(user_id: current_user.id)
+  end
 end
-
