2d64dad868a45d13a20c7aad2087415d67104fa1
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-20 22:58:10 +0900

    未ログインでもトップページは表示できるよう修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+   
    def index
       @items = Item.all
    end
@@ -21,7 +21,6 @@ class ItemsController < ApplicationController
 
    def item_params
       params.require(:item).permit(:name, :info, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :scheduled_id, :price, :image).merge(user_id: current_user.id)
-   end
-   
+   end   
 end
 
