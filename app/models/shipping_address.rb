98d1602b4394863488dbe6b063ec38b23913d304
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-27 16:43:33 +0900

    バリデーションとアソシエーションの設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,2 +1,14 @@
 class ShippingAddress < ApplicationRecord
+  belongs_to :order
+
+  validates :postal_code,     presence: true
+  validates :prefecture_id,   presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city,            presence: true
+  validates :addresses,       presence: true
+  validates :building
+  validates :phone_number,    presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
 end
