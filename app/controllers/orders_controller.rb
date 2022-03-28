2d05ba6d519755340bd0ba93b69a9b1cad35de14
TakuKunimatsu <97439488+TakuKunimatsu@users.noreply.github.com>
2022-03-28 19:52:39 +0900

    「Formオブジェクトを用いた購入情報の登録機能」と「PAY.JPを用いたクレジットカードでの決済機能」の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :move_to_index
 
   def index
     @order_shipping_address = OrderShippingAddress.new
@@ -7,11 +9,12 @@ class OrdersController < ApplicationController
 
   def create
     @order_shipping_address = OrderShippingAddress.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order_shipping_address.valid?
+      pay_item
       @order_shipping_address.save
-      redirect_to root_path
+      return redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -19,7 +22,22 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id)
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def pay_item
+    Payjp.api_key =  ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price, 
+      card: order_params[:token],    
+      currency: 'jpy'              
+    )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if @item.user.id == current_user.id || @item.order.present?
+      redirect_to controller: :items, action: :index
+    end
+  end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,8 +1,9 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :user_id, :item_id, :token
 
-
+  validates :token, presence: true
+  
   with_options presence: true do
     validates :postal_code,     format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id,   numericality: { other_than: 1, message: "can't be blank" }
@@ -15,6 +16,6 @@ class OrderShippingAddress
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, order_id: order.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
